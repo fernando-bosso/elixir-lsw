23d0894ec115753e61e28b48dcfc587648bcbcb7
Luiz <lfernando.bosso@gmail.com>
2021-04-20 08:32:42 -0300

    Corrige configuraçao db test

--- inmana/config/test.exs
+++ inmana/config/test.exs
@@ -9,7 +9,7 @@ config :inmana, Inmana.Repo,
   username: "postgres",
   password: "postgres",
   database: "inmana_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
+  hostname: "db",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
